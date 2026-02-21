95b8b8c3a893d3a515ae9d361fb43c04b2325eb6
saikrishnaproject04-glitch <saikrishna.project04@gmail.com>
2026-02-21 22:23:16 +0530

    input output

--- basic_exercises/input_output/input_output.py
+++ basic_exercises/input_output/input_output.py
@@ -1,2 +1,14 @@
 name=input("Enter your name: ")
 print('My name is',name)
+
+
+num=int(input("Enter a number: "))
+print('given number is',num)
+
+
+bool=bool(input("Enter a boolean: "))
+print('given boolean is',bool)
+
+
+float=float(input("Enter a float: "))
+print('given float is',float)
