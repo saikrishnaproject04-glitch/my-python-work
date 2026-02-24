5d08493dffebb4234e9230319933b72cba6f05f2
saikrishnaproject04-glitch <saikrishna.project04@gmail.com>
2026-02-24 18:09:08 +0530

    finding_largest_number and smallest number

---
+++ basic_exercises/largest_smallest/finding_smallest_number.py
@@ -0,0 +1,14 @@
+a=int(input('enter a number'))
+b=int(input('enter a number'))
+c=int(input('enter a number'))
+smallest=a
+if b<a:
+    smallest=b
+if c<a:
+    smallest=c
+print('smallest number is:',smallest)
+
+
+
+
+
