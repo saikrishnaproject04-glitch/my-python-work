427427acde0d0a09ef665be20401227f866ab040
saikrishnaproject04-glitch <saikrishna.project04@gmail.com>
2026-02-24 18:30:42 +0530

    finding simple calculator

---
+++ basic_exercises/simple_calculator/findind_calculator.py
@@ -0,0 +1,9 @@
+x=float(input('enter a number:'))
+y=float(input('enter a number:'))
+print('sum:',x+y)
+print('difference:',x-y)
+print('multi:',x*y)
+if y!=0:
+    print('division:',x/y)
+else:
+    print('not divided by zero')
